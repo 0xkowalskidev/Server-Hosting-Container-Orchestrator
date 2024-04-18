d32a10370e4e6b2f543cd03fc0fbc90d11269c3b
0xKowalski <0xkowalskiaudit@gmail.com>
2024-04-18 05:39:22 +0100

    Finished storage manager tests, added rollbacks if create volume fails

--- tests/storage/storage_test.go
+++ tests/storage/storage_test.go
@@ -44,7 +44,7 @@ func TestStorageManager_RemoveVolume(t *testing.T) {
 	mockCmdRunner.AssertExpectations(t)
 }
 
-func TestStorageManager_RemoveNonExistantVolume(t *testing.T) {
+func TestStorageManager_RemoveVolume_NonExistantVolume(t *testing.T) {
 	sm, mockFileOps, _ := setup()
 
 	volumeID := "noVolume"
@@ -105,6 +105,113 @@ func TestStorageManager_CreateVolume(t *testing.T) {
 	assert.Equal(t, expectedVolume.ID, volume.ID)
 	assert.Equal(t, mountPoint, volume.MountPoint)
 
+	mockFileOps.AssertExpectations(t)
+	mockCmdRunner.AssertExpectations(t)
+}
+
+// CreateVolume rollbacks
+func TestStorageManager_CreateVolume_FailureAtCreateFixedSizeFile(t *testing.T) {
+	sm, mockFileOps, mockCmdRunner := setup()
+
+	volumeID := "volume1"
+	mountPoint := fmt.Sprintf("%s/%s", fakeStoragePath, volumeID)
+	imageMountPoint := fmt.Sprintf("%s.img", mountPoint)
+
+	mockFileOps.On("Stat", mountPoint).Return(utils_test.FakeFileInfo{}, os.ErrNotExist)
+	mockFileOps.On("MkdirAll", mountPoint, os.FileMode(0755)).Return(nil)
+	mockCmdRunner.On("RunCommand", "fallocate", "-l", "1073741824", imageMountPoint).Return(fmt.Errorf("fallocate failed"))
+
+	// Expect RemoveAll to be called to clean up the created directory
+	mockFileOps.On("RemoveAll", mountPoint).Return(nil)
+
+	_, err := sm.CreateVolume(volumeID, 1)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "fallocate failed")
+
+	mockFileOps.AssertExpectations(t)
+	mockCmdRunner.AssertExpectations(t)
+}
+
+func TestStorageManager_CreateVolume_FailureAtFormatAsExt4(t *testing.T) {
+	sm, mockFileOps, mockCmdRunner := setup()
+
+	volumeID := "volume1"
+	mountPoint := fmt.Sprintf("%s/%s", fakeStoragePath, volumeID)
+	imageMountPoint := fmt.Sprintf("%s.img", mountPoint)
+
+	mockFileOps.On("Stat", mountPoint).Return(utils_test.FakeFileInfo{}, os.ErrNotExist)
+	mockFileOps.On("MkdirAll", mountPoint, os.FileMode(0755)).Return(nil)
+	mockCmdRunner.On("RunCommand", "fallocate", "-l", "1073741824", imageMountPoint).Return(nil)
+	mockCmdRunner.On("RunCommand", "mkfs.ext4", imageMountPoint).Return(fmt.Errorf("mkfs.ext4 failed"))
+
+	// Expectations for rollback actions
+	mockFileOps.On("Remove", imageMountPoint).Return(nil)
+	mockFileOps.On("RemoveAll", mountPoint).Return(nil)
+
+	_, err := sm.CreateVolume(volumeID, 1)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "mkfs.ext4 failed")
+
+	mockFileOps.AssertExpectations(t)
+	mockCmdRunner.AssertExpectations(t)
+}
+
+func TestStorageManager_CreateVolume_FailureAtMountVolume(t *testing.T) {
+	sm, mockFileOps, mockCmdRunner := setup()
+
+	volumeID := "volume1"
+	mountPoint := fmt.Sprintf("%s/%s", fakeStoragePath, volumeID)
+	imageMountPoint := fmt.Sprintf("%s.img", mountPoint)
+
+	mockFileOps.On("Stat", mountPoint).Return(utils_test.FakeFileInfo{}, os.ErrNotExist)
+	mockFileOps.On("MkdirAll", mountPoint, os.FileMode(0755)).Return(nil)
+	mockCmdRunner.On("RunCommand", "fallocate", "-l", "1073741824", imageMountPoint).Return(nil)
+	mockCmdRunner.On("RunCommand", "mkfs.ext4", imageMountPoint).Return(nil)
+	mockCmdRunner.On("RunCommand", "mount", "-o", "loop", imageMountPoint, mountPoint).Return(fmt.Errorf("mount failed"))
+
+	// Expectations for rollback actions
+	mockFileOps.On("Remove", imageMountPoint).Return(nil)
+	mockFileOps.On("RemoveAll", mountPoint).Return(nil)
+
+	_, err := sm.CreateVolume(volumeID, 1)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "mount failed")
+
+	mockFileOps.AssertExpectations(t)
+	mockCmdRunner.AssertExpectations(t)
+}
+
+func TestStorageManager_CreateVolume_FailureAtRemovingLostAndFound(t *testing.T) {
+	sm, mockFileOps, mockCmdRunner := setup()
+
+	volumeID := "volume1"
+	mountPoint := fmt.Sprintf("%s/%s", fakeStoragePath, volumeID)
+	imageMountPoint := fmt.Sprintf("%s.img", mountPoint)
+	lostFoundPath := fmt.Sprintf("%s/lost+found", mountPoint)
+
+	// Setup to pass all previous steps
+	mockFileOps.On("Stat", mountPoint).Return(utils_test.FakeFileInfo{}, os.ErrNotExist)
+	mockFileOps.On("MkdirAll", mountPoint, os.FileMode(0755)).Return(nil)
+	mockCmdRunner.On("RunCommand", "fallocate", "-l", "1073741824", imageMountPoint).Return(nil)
+	mockCmdRunner.On("RunCommand", "mkfs.ext4", imageMountPoint).Return(nil)
+	mockCmdRunner.On("RunCommand", "mount", "-o", "loop", imageMountPoint, mountPoint).Return(nil)
+
+	// Simulate failure in removing the lost+found directory
+	mockFileOps.On("RemoveAll", lostFoundPath).Return(fmt.Errorf("failed to remove lost+found"))
+
+	// Expectations for rollback actions
+	mockCmdRunner.On("RunCommand", "umount", mountPoint).Return(nil) // Expect unmount to be called during rollback
+	mockFileOps.On("Remove", imageMountPoint).Return(nil)            // Expect file removal during rollback
+	mockFileOps.On("RemoveAll", mountPoint).Return(nil)              // Expect directory removal during rollback
+
+	// Execute the function that's being tested
+	_, err := sm.CreateVolume(volumeID, 1)
+
+	// Assert expected error and message
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "failed to remove lost+found")
+
+	// Verify that all expectations were met
 	mockFileOps.AssertExpectations(t)
 	mockCmdRunner.AssertExpectations(t)
 }
