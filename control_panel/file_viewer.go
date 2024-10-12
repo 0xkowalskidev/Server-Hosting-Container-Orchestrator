4215a4fb77f0ec441918ff1ad214f6723e1b83c0
0xkowalskidev <0xkowalskidev@gmail.com>
2024-10-12 20:58:58 +0100

    Added dir navigation and breadcrumbs

--- control_panel/file_viewer.go
+++ control_panel/file_viewer.go
@@ -1,6 +1,7 @@
 package controlpanel
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
@@ -26,7 +27,8 @@ func NewFileViewer() *FileViewer {
 
 func (fv *FileViewer) HandleFileViewer(c *websocket.Conn) {
 	id := c.Params("id")
-	log.Println(id)
+	log.Println("Connected to gameserver ID:", id)
+
 	sftpClient, err := fv.connectSFTP(id)
 	if err != nil {
 		log.Println("Failed to connect to SFTP:", err)
@@ -35,9 +37,9 @@ func (fv *FileViewer) HandleFileViewer(c *websocket.Conn) {
 	}
 	defer sftpClient.Close()
 
+	// Send initial directory listing on connection
 	fv.handleListDir(c, sftpClient, ".")
 
-	// Continuously listen for incoming messages and handle them
 	for {
 		_, msg, err := c.ReadMessage()
 		if err != nil {
@@ -45,11 +47,21 @@ func (fv *FileViewer) HandleFileViewer(c *websocket.Conn) {
 			break
 		}
 
-		messageParts := strings.SplitN(string(msg), " ", 2)
-		command := messageParts[0]
-		path := "."
-		if len(messageParts) > 1 {
-			path = messageParts[1]
+		var data map[string]interface{}
+		if err := json.Unmarshal(msg, &data); err != nil {
+			log.Printf("Failed to parse JSON: %v", err)
+			continue
+		}
+
+		var command, path string
+		for key, value := range data {
+			if key == "HEADERS" {
+				continue // Skip headers
+			}
+			command = key
+			if val, ok := value.(string); ok {
+				path = val
+			}
 		}
 
 		if handler, ok := fv.commandHandlers[command]; ok {
@@ -89,11 +101,36 @@ func (fv *FileViewer) handleListDir(c *websocket.Conn, client *sftp.Client, path
 	}
 
 	var htmlResponse strings.Builder
-	htmlResponse.WriteString("<ul id='files'>")
-	for _, file := range files {
-		htmlResponse.WriteString(fmt.Sprintf("<li>%s</li>", file.Name()))
+	// Breadcrumbs
+	htmlResponse.WriteString("<ul id='breadcrumbs'>")
+	parts := strings.Split(path, "/")
+	for i := range parts {
+		if i == 0 && parts[i] == "." {
+			htmlResponse.WriteString(`<li name='ls' value='.'>Home</li>`)
+		} else if parts[i] != "" {
+			partialPath := strings.Join(parts[:i+1], "/")
+			htmlResponse.WriteString(fmt.Sprintf(
+				`<li name='ls' value='%s'>%s</li>`,
+				partialPath, parts[i],
+			))
+		}
 	}
 	htmlResponse.WriteString("</ul>")
+
+	// File Form
+	htmlResponse.WriteString("<form id='files' hx-trigger='click' ws-send='true'>") // Create a form with ws-send
+
+	for _, file := range files {
+		if file.IsDir() {
+			htmlResponse.WriteString(fmt.Sprintf(`
+                <button type='submit' name='ls' value='%s/%s'>%s/</button><br>`,
+				path, file.Name(), file.Name(),
+			))
+		} else {
+			htmlResponse.WriteString(fmt.Sprintf("<span>%s</span><br>", file.Name()))
+		}
+	}
+	htmlResponse.WriteString("</form>") // Close the form
 
 	if err = c.WriteMessage(websocket.TextMessage, []byte(htmlResponse.String())); err != nil {
 		log.Println("write:", err)
